15550a9a55035b1f30af4f590addd759be266678
roman-boiko <roman.boiko@gmail.com>
2025-05-21 18:47:29 +0200

    Added generating short url and saving it in sqlight database.

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
-from fastapi import FastAPI, HTTPException
+import secrets
+from fastapi import FastAPI, HTTPException, Depends
 import schemas
+import models
+from sqlalchemy.orm import Session
 import validators
+from database import SessionLocal, engine
 
 app = FastAPI()
+models.Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    """
+    Dependency to get a database session.
+    """
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 def raise_bad_request(message: str):
@@ -17,14 +33,21 @@ async def root():
     return {"message": "Welcome to shortenURL API!"}
 
 
-@app.post("/url")
-def create_url(url: schemas.URLBase):
+@app.post("/url", response_model=schemas.URLInfo)
+def create_url(url: schemas.URLBase, db: Session = Depends(get_db)):
     """
     Create a new URL entry.
     """
     if not validators.url(url.target_url):
         raise_bad_request("Invalid URL format.")
+    chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    key = "".join(secrets.choice(chars) for _ in range(5))
+    secret_key = "".join(secrets.choice(chars) for _ in range(8))
+    db_url = models.URL(target_url=url.target_url, key=key, secret_key=secret_key)
+    db.add(db_url)
+    db.commit()
+    db.refresh(db_url)
+    db_url.url = key
+    db_url.admin_url = secret_key
 
-    # Here you would typically save the URL to a database
-    # For demonstration, we will just return the URL
-    return {"target_url": url.target_url, "is_active": True, "clicks": 0}
+    return db_url
--- schemas.py
+++ schemas.py
@@ -10,7 +10,7 @@ class URL(URLBase):
     clicks: int
 
     class Config:
-        orm_mode = True
+        from_attributes = True
 
 
 class URLInfo(URL):
